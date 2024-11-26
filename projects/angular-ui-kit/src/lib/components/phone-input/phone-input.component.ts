131417478bbef47d7d979c4f46f3b265f7880183
momen-at-markazia <momen_alshouha@ctajo.com>
2024-11-26 15:29:20 +0300

    password, datepicker, phone input style enhancment

--- projects/angular-ui-kit/src/lib/components/phone-input/phone-input.component.ts
+++ projects/angular-ui-kit/src/lib/components/phone-input/phone-input.component.ts
@@ -11,36 +11,10 @@ interface Country {
 @Component({
   selector: 'lib-phone-input',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './phone-input.component.html',
-  styleUrl: './phone-input.component.scss'
+  styleUrls: ['./phone-input.component.scss']
 })
 export class PhoneInputComponent {
-  @Input() placeholder: string = 'Enter your phone number';
-  @Output() phoneChange = new EventEmitter<string>();
-
-  countries: Country[] = [
-    { name: 'United States', code: '+1', flag: '🇺🇸' },
-    { name: 'Canada', code: '+1', flag: '🇨🇦' },
-    { name: 'United Kingdom', code: '+44', flag: '🇬🇧' },
-    { name: 'Germany', code: '+49', flag: '🇩🇪' },
-    { name: 'France', code: '+33', flag: '🇫🇷' },
-    { name: 'Jordan', code: '+962', flag: '🇯🇴' },
-    { name: 'Saudi Arabia', code: '+966', flag: '🇸🇦' },
-  ];
-
-  selectedCountry: Country = this.countries[0];
-  phoneNumber: string = '';
-
-  // Update the selected country
-  onCountryChange(event: Event): void {
-    const selectElement = event.target as HTMLSelectElement;
-    const selectedValue = selectElement.value;
-    console.log('Selected country:', selectedValue);
-  }  
-
-  // Emit phone number change
-  onPhoneNumberChange(event: any) {
-    this.phoneChange.emit(this.selectedCountry.code + this.phoneNumber);
-  }
+ 
 }
